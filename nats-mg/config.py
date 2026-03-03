9e8be2b498b2d21fba973e6d42dd1a0a25f1523b
alermar69 <alermar69@mail.ru>
2026-03-03 11:32:15 +0200

    fix(config): исправлен путь к базе директории
    Уменьшена глубина разрешения пути в `BASE_DIR` с `parent.parent` на `parent` для корректного определения корневой директории проекта.
    
    chore(ci): удалена избыточная команда логирования
    
    Убрана команда `docker compose logs` из CI-скрипта, так как она не требуется для успешного деплоя.

--- nats-mg/config.py
+++ nats-mg/config.py
@@ -6,7 +6,7 @@ from pydantic_settings import (
     SettingsConfigDict,
 )
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 LOG_DEFAULT_FORMAT = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
